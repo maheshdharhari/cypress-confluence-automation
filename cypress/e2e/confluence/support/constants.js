ab81be81cb202fe946c7a2d41e1f5da3116ba177
Mahesh Kumar Yadav <contact@maheshyadav.com.np>
2025-04-01 17:38:45 +0545

    add constants documentation for SPACE_KEY and PARENT_PAGE_ID in constants.js

--- cypress/e2e/confluence/support/constants.js
+++ cypress/e2e/confluence/support/constants.js
@@ -1,2 +1,11 @@
+/**
+ * The key of the Confluence space where pages will be created or managed.
+ * @constant {string} SPACE_KEY
+ */
+
+/**
+ * The ID of the parent page under which new pages will be created in Confluence.
+ * @constant {string} PARENT_PAGE_ID
+ */
 export const SPACE_KEY = 'mahesh'
 export const PARENT_PAGE_ID = '51020205'
