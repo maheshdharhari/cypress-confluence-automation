c9e2add3bbf666778c39f772e407f0098860c580
Mahesh Kumar Yadav <contact@maheshyadav.com.np>
2025-04-01 17:59:44 +0545

    fix: update baseUrl in cypress.config.js to a hardcoded value for testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CONFLUENCE_URL,
+    baseUrl: 'http://confluence:8612',
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
     viewportWidth: 1920,
