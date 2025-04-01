77ce5100549525b64c911f77ddb1e0d0fdd15dfb
Mahesh Kumar Yadav <contact@maheshyadav.com.np>
2025-04-01 17:37:40 +0545

    add documentation to e2e.js for custom Cypress command imports

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,12 @@
+/**
+ * This file serves as a support file for Cypress end-to-end tests.
+ * 
+ * It imports custom Cypress commands from two locations:
+ * 1. `./commands` - Local commands specific to the current project.
+ * 2. `../e2e/confluence/support/commands` - Commands related to Confluence-specific test automation.
+ * 
+ * These imports extend Cypress's functionality, enabling the use of custom commands
+ * throughout the test suite.
+ */
 import './commands'
 import '../e2e/confluence/support/commands'
